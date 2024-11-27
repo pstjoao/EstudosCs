0a0abbe1df07db6734519c826945f6b6413f634c
João Pedro Turi <jpturi14@gmail.com>
2024-11-27 06:10:23 -0300

    .

--- SaidaDeDados/SaidaDeDados/Program.cs
+++ SaidaDeDados/SaidaDeDados/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SaidaDeDados {
+    class Program {
+        static void Main(string[] args) {
+
+            char genero = 'F';
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "José";
+
+            Console.Write("Bom dia!");
+            Console.WriteLine("Boa tarde!");
+            Console.WriteLine("-------------");
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo);
+            Console.WriteLine(nome);
+            Console.WriteLine(saldo.ToString("F2"));
+        }
+    }
+}
