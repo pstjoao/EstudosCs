0d9f3730f0a2be2d4f4c79e7fd42c5ce878d1d21
João Pedro Turi <jpturi14@gmail.com>
2024-11-29 17:57:05 -0300

    Exercício sobre saída de dados

---
+++ ExercicioSaidaDeDados/ExercicioSaidaDeDados/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioSaidaDeDados {
+    internal class ExercicioSaideDeDados {
+        static void Main(string[] args) {
+
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos:");
+            Console.WriteLine("{0}, cujo preço é ${1:F2}", produto1, preco1);
+            Console.WriteLine("{0}, cujo preço é ${1:F2}", produto2 , preco2);
+            Console.WriteLine("");
+            Console.WriteLine("Registro: {0} anos de idade, código {1} e gênero: {2}", idade, codigo, genero);
+            Console.WriteLine("");
+            Console.WriteLine("Medida com oito casas decimais: " + (medida));
+            Console.WriteLine("Arredondado (três casas decimais): " + (medida.ToString("F3")));
+            Console.WriteLine("Separador decimal invariant culture: " + (medida.ToString("F3",CultureInfo.InvariantCulture)));
+        }
+    }
+}
