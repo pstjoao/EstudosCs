7775e7ca932266d075d417e89551c2bf74dd0efe
João Pedro Turi <jpturi14@gmail.com>
2024-12-01 14:22:42 -0300

    Estudos sobre conversão implícita e casting

---
+++ ConversaoCasting/ConversaoCasting/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConversaoCasting {
+    internal class Program {
+        static void Main(string[] args) {
+
+            double x;
+            float y;
+
+            x = 5.1;
+            y = (float)x;
+            Console.WriteLine(y);
+
+            double m;
+            int n;
+
+            m = 5.1;
+            n = (int)m;
+            Console.WriteLine(n);
+
+            int a = 5;
+            int b = 2;
+
+            double resultado = (double) a / b;
+            Console.WriteLine(resultado);  
+        }
+    }
+}
