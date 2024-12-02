eaa7b183438ae01b64cfd1573b88157cf82fefab
João Pedro Turi <jpturi14@gmail.com>
2024-12-02 19:53:27 -0300

    .

---
+++ QuadraticEquation/QuadraticEquation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace QuadraticEquation {
+    internal class Program {
+        static void Main(string[] args) {
+
+            double a = 1.0, b = -3.0, c = -4.0;
+
+            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+
+            double resultado1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+            double resultado2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+            Console.WriteLine(delta);
+            Console.WriteLine(resultado1);
+            Console.WriteLine(resultado2);
+        }
+    }
+}
