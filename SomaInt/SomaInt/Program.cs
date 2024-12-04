a37e4660529c29c878c0908ebb7ddd8bcf00726f
João Pedro Turi <jpturi14@gmail.com>
2024-12-04 19:57:02 -0300

    Exercício de soma

---
+++ SomaInt/SomaInt/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SomaInt {
+    internal class Program {
+        static void Main(string[] args) {
+            
+            Console.WriteLine("Insira o valor 1:");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o valor 2:");
+            int y = int.Parse(Console.ReadLine());
+
+            int result = x + y;
+
+            Console.WriteLine("SOMA = " + result);
+        }
+    }
+}
