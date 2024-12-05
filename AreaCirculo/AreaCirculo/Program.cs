d6dea72111402b59d813c3c28c6753e32ebd6564
João Pedro Turi <jpturi14@gmail.com>
2024-12-04 22:06:13 -0300

    Exercício área de um círculo

---
+++ AreaCirculo/AreaCirculo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace AreaCirculo {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Insira o valor do raio:");
+            double r = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double area = Math.Pow(r, 2) * 3.14159;
+
+            Console.WriteLine("A área é: ");
+            Console.WriteLine(area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
