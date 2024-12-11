b3d66fbc90a188d1baa0fdcf15b7da6b62d80029
João Pedro Turi <jpturi14@gmail.com>
2024-12-10 22:21:00 -0300

    Estudos estrutura condicional

---
+++ EstruturaCondicional/EstruturaCondicional/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace EstruturaCondicional {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int x = 10;
+
+            Console.WriteLine("Bom dia");
+
+            if (x < 5) {
+                Console.WriteLine("Boa tarde");
+            }
+
+            Console.WriteLine("Boa noite");
+
+            Console.WriteLine("---------------------");
+
+            Console.WriteLine("Entre com um número inteiro:");
+            int y = int.Parse(Console.ReadLine());
+
+            if (y % 2 == 0) {
+                Console.WriteLine("Par!");
+            }
+            else {
+                Console.WriteLine("Ímpar!");
+            }
+
+            Console.WriteLine("---------------------");
+
+            Console.WriteLine("Qual a hora atual?");
+            int h = int.Parse(Console.ReadLine());
+
+            if (h < 12) {
+                Console.WriteLine("Bom dia!");
+            }
+            else if (h < 18) {
+                Console.WriteLine("Boa tarde!");
+            }
+            else {
+                Console.WriteLine("Boa noite!");
+            }
+        }
+    }
+}
