6f6211aa7f91816fe6631aceb9852ac2acfa9f00
João Pedro Turi <jpturi14@gmail.com>
2024-12-12 11:11:36 -0300

    Exercício if-else

---
+++ Exifelse01/Exifelse01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exifelse01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Insira um número inteiro:");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num < 0 ) {
+                Console.WriteLine("NEGATIVO");
+            }
+            else {
+                Console.WriteLine("NÃO NEGATIVO");
+            }
+        }
+    }
+}
