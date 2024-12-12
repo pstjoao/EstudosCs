4b6bb6d2f39cf6291d1ad75ea589d45840cba60a
João Pedro Turi <jpturi14@gmail.com>
2024-12-12 11:36:36 -0300

    Exercícios if-else

---
+++ Exifelse02/Exifelse02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exifelse02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Insira um número inteiro:");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0) {
+                Console.WriteLine("O número é PAR");
+            }
+            else {
+                Console.WriteLine("O número é ÍMPAR");
+            }
+        }
+    }
+}
---
+++ Exifelse03/Exifelse03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exifelse03 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Insira um valor inteiro A:");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira um valor inteiro B:");
+            int b = int.Parse(Console.ReadLine());
+
+            if (a == 1 && b != 1) {
+                Console.WriteLine("B é múltiplo de A");
+            }
+            else if (b == 1 && a != 1) {
+                Console.WriteLine("A é múltiplo de B");
+            }
+            else if  (a % b == 0 || b % a == 0) {
+                Console.WriteLine("São múltiplos");
+            }
+            else {
+                Console.WriteLine("Não são múltiplos");
+            }
+        }
+    }
+}
