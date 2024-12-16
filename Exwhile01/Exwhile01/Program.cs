e1c6bade3ffa17bccb8a36926c634b5cea2c4bb4
João Pedro Turi <jpturi14@gmail.com>
2024-12-15 21:01:43 -0300

    Exercício utilizando while

---
+++ Exwhile01/Exwhile01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exwhile01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Digite a senha: ");
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002) {
+                Console.WriteLine("Senha Inválida. Tente novamente");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
