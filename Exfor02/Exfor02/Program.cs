db1c3eb074b75fbebacce4d1ee94130528670786
João Pedro Turi <jpturi14@gmail.com>
2024-12-16 09:19:26 -0300

    Exercício utilizando for

---
+++ Exfor02/Exfor02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exfor02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Insira um valor inteiro N: ");
+            int N = int.Parse(Console.ReadLine());
+            int dentro = 0;
+            int fora = 0;
+
+            for (int i = 1; i <= N; i++) {
+               int n = int.Parse(Console.ReadLine());
+               if (n >= 10 && n <= 20) {
+                    dentro += 1;
+               }
+               else {
+                    fora += 1;
+                }
+            }
+
+            Console.WriteLine(dentro + " in");
+            Console.WriteLine(fora + " out");
+        }
+    }
+}
