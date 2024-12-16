8676f9e0bdba2dffefec1f01f882b788f74fc566
João Pedro Turi <jpturi14@gmail.com>
2024-12-16 10:08:37 -0300

    Exercício utilizando for

---
+++ Exfor06/Exfor06/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exfor06 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Insira um número: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Os divisores são: ");
+            for (int i = 1; i <= num; i++) {
+                if (num % i == 0) {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
