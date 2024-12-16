dccd4f1a97cf9777eccf7153ad910978b12336a8
João Pedro Turi <jpturi14@gmail.com>
2024-12-16 09:57:58 -0300

    Exercício utilizando for

---
+++ Exfor05/Exfor05/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exfor05 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Insira o valor: ");
+            int n = int.Parse(Console.ReadLine());
+            int fatorial = 1;
+
+            for (int i = 1; i <= n; i++) {
+                int valor = i;
+                fatorial *= valor;
+            }
+            Console.WriteLine(fatorial);
+        }
+    }
+}
