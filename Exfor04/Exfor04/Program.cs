a02dba3aea7cbb222c1d9ab8015448edcd1828db
João Pedro Turi <jpturi14@gmail.com>
2024-12-16 09:47:03 -0300

    Exercício utilizando for

---
+++ Exfor04/Exfor04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exfor04 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Insira o número de pares de números que serão lidos: ");
+            int pares = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= pares; i++) {
+                string[] valores = Console.ReadLine().Split(' ');
+                double n1 = double.Parse(valores[0], CultureInfo.InvariantCulture);
+                double n2 = double.Parse(valores[1], CultureInfo.InvariantCulture);
+
+                if (n2 != 0) { 
+                double divisao = n1 / n2;
+                Console.WriteLine(divisao.ToString("F3", CultureInfo.InvariantCulture));
+                }
+                else {
+                    Console.WriteLine("Divisão impossível");
+                }
+            }
+        }
+    }
+}
