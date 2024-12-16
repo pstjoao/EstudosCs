368e9c396aeb320d58462c3c0c314c3a8143b604
João Pedro Turi <jpturi14@gmail.com>
2024-12-16 09:35:12 -0300

    Exercício utilizando for

---
+++ Exfor03/Exfor03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exfor03 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Insira o número de casos que serão analisados: ");
+            int casos = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= casos; i++) {
+                string[] valores = Console.ReadLine().Split(' ');
+                double v1 = double.Parse(valores[0], CultureInfo.InvariantCulture);
+                double v2 = double.Parse(valores[1], CultureInfo.InvariantCulture);
+                double v3 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+                double media = (2*v1 + 3*v2 + 5*v3) / (2 + 3 + 5);
+                Console.WriteLine("Média ponderada: " + media.ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
