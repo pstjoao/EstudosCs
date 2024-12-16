83bd46a4b987dc210b90feaa5eccb07f1440fb31
João Pedro Turi <jpturi14@gmail.com>
2024-12-16 10:17:13 -0300

    Exercício utilizando for

---
+++ Exfor07/Exfor07/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exfor07 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Insira um valor positivo: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                Console.Write(i + " ");
+                Console.Write(Math.Pow(i, 2) + " ");
+                Console.WriteLine(Math.Pow(i, 3) + " ");
+            }
+        }
+    }
+}
