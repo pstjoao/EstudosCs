ff46467a47480a2b0e392f55f73b133854b5ba18
João Pedro Turi <jpturi14@gmail.com>
2024-12-16 08:54:56 -0300

    Exercício utilizando for

---
+++ Exfor01/Exfor01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exfor01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Insira um valor de 1 a 1000: ");
+            int valor = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= valor; i += 2) {
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
