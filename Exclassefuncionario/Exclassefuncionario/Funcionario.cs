3c5501773fc76286d191b8de41aa6862a899fbbb
João Pedro Turi <jpturi14@gmail.com>
2024-12-18 18:10:15 -0300

    Exercício classes sobre funcionário

---
+++ Exclassefuncionario/Exclassefuncionario/Funcionario.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace Exclassefuncionario {
+    internal class Funcionario {
+
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido() {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcent) {
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcent / 100.0);
+        }
+
+        public override string ToString() {
+            return Nome + ", $ " + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
