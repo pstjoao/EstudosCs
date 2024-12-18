84b03b88eb6b1fd40163a74346b5ad56d665d777
João Pedro Turi <jpturi14@gmail.com>
2024-12-18 12:45:06 -0300

    Exercício produtos add e rem

---
+++ Exclasseprodutos/Exclasseprodutos/Produto.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace Exclasseprodutos {
+    internal class Produto {
+
+        public string Nome;
+        public double Preco;
+        public int Quant;
+
+        public double ValorTotalEmEstoque() {
+            return Preco * Quant;
+        }
+
+        public void AdicionarProdutos(int quantidade) {
+            Quant += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade) {
+            Quant -= quantidade;
+        }
+
+        public override string ToString() {
+            return Nome 
+                + ", $ " 
+                + Preco.ToString("F2", CultureInfo.InvariantCulture) 
+                + ", " + Quant 
+                + " unidades, Total: $ " 
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
