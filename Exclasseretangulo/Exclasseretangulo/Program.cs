bd63b148d7a2896c75cee7b7734213bdc4e302f4
João Pedro Turi <jpturi14@gmail.com>
2024-12-18 14:55:32 -0300

    Exercício classes sobre retângulos

---
+++ Exclasseretangulo/Exclasseretangulo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exclasseretangulo {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Retangulo ret = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo:");
+            ret.Largura = double.Parse(Console.ReadLine());
+            ret.Altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("ÁREA = " + ret.Area());
+            Console.WriteLine("PERÍMETRO = " + ret.Perimetro());
+            Console.WriteLine("DIAGONAL = " + ret.Diagonal());
+        }
+    }
+}
---
+++ Exclasseretangulo/Exclasseretangulo/Retangulo.cs
@@ -0,0 +1,21 @@
+﻿namespace Exclasseretangulo {
+    internal class Retangulo {
+
+        public double Largura;
+        public double Altura;
+
+        public double Area() {
+            return Largura * Altura;
+        }
+
+        public double Perimetro() {
+            return 2 * (Largura + Altura);
+        }
+
+        public double Diagonal() {
+            double Diagonal;
+            Diagonal = Math.Sqrt(Math.Pow(Largura, 2) + Math.Pow(Altura, 2));
+            return Diagonal;
+        }
+    }
+}
