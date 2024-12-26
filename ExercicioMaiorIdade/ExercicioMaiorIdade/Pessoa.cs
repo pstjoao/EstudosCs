6d907ad80258a010589def37229079a21525d56a
João Pedro Turi <jpturi14@gmail.com>
2024-12-26 15:16:52 -0300

    Exercício sobre maior idade com POO

---
+++ ExercicioMaiorIdade/ExercicioMaiorIdade/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿namespace ExercicioMaiorIdade {
+    internal class Pessoa {
+
+        public Pessoa() {
+            Nome = "";
+            Idade = 0;
+        }
+
+        public Pessoa(string nome, int idade) {
+            Nome = nome;
+            Idade = idade;
+        }
+
+        public string Nome;
+        public int Idade;
+
+        public void ExibirDados() {
+            Console.WriteLine("Nome: " + Nome);
+            Console.WriteLine("Idade: " + Idade);
+        }
+    }
+}
---
+++ ExercicioMaiorIdade/ExercicioMaiorIdade/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ExercicioMaiorIdade {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
+            Pessoa p3 = new Pessoa();
+
+            Console.Write("Nome da 1 pessoa: ");
+            p1.Nome = Console.ReadLine();
+            Console.Write("Idade da 1 pessoa: ");
+            p1.Idade = int.Parse(Console.ReadLine());
+
+            Console.Write("Nome da 2 pessoa: ");
+            p2.Nome = Console.ReadLine();
+            Console.Write("Idade da 2 pessoa: ");
+            p2.Idade = int.Parse(Console.ReadLine());
+
+            Console.Write("Nome da 3 pessoa: ");
+            string nome = Console.ReadLine();
+            Console.Write("Idade da 3 pessoa: ");
+            int idade = int.Parse(Console.ReadLine());
+            p3 = new Pessoa(nome, idade);
+
+            if (p1.Idade > p2.Idade && p1.Idade > p3.Idade) {
+                Console.WriteLine("A pessoa mais velha é: ");
+                p1.ExibirDados();
+            }
+            else if (p2.Idade > p1.Idade && p2.Idade > p3.Idade) {
+                Console.WriteLine("A pessoa mais velha é: ");
+                p2.ExibirDados();
+            }
+            else if (p3.Idade > p1.Idade && p3.Idade > p2.Idade) {
+                Console.WriteLine("A pessoa mais velha é: ");
+                p3.ExibirDados();
+            }
+            else {
+                Console.WriteLine("Todas as pessoas possuem a mesma idade");
+            }
+        }
+    }
+}
