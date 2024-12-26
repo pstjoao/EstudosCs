4df79c5cd42aed82e29f81724d3d844255fcfccb
João Pedro Turi <jpturi14@gmail.com>
2024-12-26 15:41:33 -0300

    Exercício área de um retângulo com POO

---
+++ ExercicioAreaRetangulo/ExercicioAreaRetangulo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExercicioAreaRetangulo {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Retangulo r = new Retangulo();
+
+            Console.WriteLine("Informe a altura do retângulo: ");
+            r.Altura = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a largura do retângulo: ");
+            r.Largura = double.Parse(Console.ReadLine());
+
+            r.ExibirDados();
+        }
+    }
+}
---
+++ ExercicioAreaRetangulo/ExercicioAreaRetangulo/Retangulo.cs
@@ -0,0 +1,17 @@
+﻿namespace ExercicioAreaRetangulo {
+    internal class Retangulo {
+
+        public double Altura;
+        public double Largura;
+
+        public double Area() {
+            return Altura * Largura;
+        }
+
+        public void ExibirDados() {
+            Console.WriteLine("Altura: " + Altura);
+            Console.WriteLine("Largura: " + Largura);
+            Console.WriteLine("Área: " + Area());
+        }
+    }
+}
