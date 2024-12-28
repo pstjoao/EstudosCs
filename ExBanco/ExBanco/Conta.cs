d3f778b20abb2f02007138afb937ec78cbe20cbc
João Pedro Turi <jpturi14@gmail.com>
2024-12-28 17:29:32 -0300

    Exercício conta bancária

---
+++ ExBanco/ExBanco/Conta.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace ExBanco {
+    internal class Conta {
+
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public Conta(int numero, string titular) {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public Conta(int numero, string titular, double saldo) : this(numero, titular) {
+            Saldo = saldo;
+        }
+
+        public void Deposito(double quantia) {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia) {
+            Saldo -= quantia + 5.0; 
+        }
+
+        public override string ToString() {
+            return "Conta " 
+                + Numero 
+                + ", Titular: " 
+                + Titular 
+                + ", Saldo: $ " 
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
