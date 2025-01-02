048ad7ab74dd0275f1f92d101abc6e749361fe28
João Pedro Turi <jpturi14@gmail.com>
2025-01-02 20:54:53 -0300

    Exercício POO Produto

---
+++ Expoo01/Expoo01/Produto.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection.Metadata;
+using System.Threading.Channels;
+
+namespace Expoo01 {
+    internal class Produto {
+
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto(string nome, double preco, int quantidade) {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public void AdicionarEstoque(int quantidade) {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverEstoque(int quantidade) {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString() {
+            return Nome 
+                + ", " 
+                + Preco 
+                + ", " 
+                + Quantidade;
+        }
+    }
+}
---
+++ Expoo01/Expoo01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Expoo01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Produto p;
+
+            Console.Write("Informe o nome do produto: ");
+            string nome = Console.ReadLine();
+            Console.Write("Informe o preço do produto: ");
+            double preco = double.Parse(Console.ReadLine());
+            Console.Write("Informe a quantidade do produto: ");
+            int quant = int.Parse(Console.ReadLine());
+
+            p = new Produto(nome, preco, quant);
+
+            Console.Write("Informe a quantidade de itens a serem adicionados: ");
+            int quantadd = int.Parse(Console.ReadLine());
+            p.AdicionarEstoque(quantadd);
+            Console.Write("Informe a quantidade de itens a serem removidos: ");
+            int quantrem = int.Parse(Console.ReadLine());
+            p.RemoverEstoque(quantrem);
+
+            Console.WriteLine(p);
+        }
+    }
+}
