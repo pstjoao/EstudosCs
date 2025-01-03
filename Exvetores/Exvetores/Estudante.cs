0e220d29d6bc2793ce40e3676114890c4e2a3a5e
João Pedro Turi <jpturi14@gmail.com>
2025-01-03 17:15:45 -0300

    Exercício com vetores

---
+++ Exvetores/Exvetores/Estudante.cs
@@ -0,0 +1,20 @@
+﻿namespace Exvetores {
+    internal class Estudante {
+
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Quarto { get; set; }
+
+        public Estudante(string nome, string email, int quarto) {
+            Nome = nome;
+            Email = email;
+            Quarto = quarto;
+        }
+
+        public override string ToString() {
+            return Nome 
+                + ", " 
+                + Email;
+        }
+    }
+}
---
+++ Exvetores/Exvetores/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exvetores {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Estudante[] vect = new Estudante[10];
+
+            for (int i = 0; i < quartos; i++) {
+                Console.WriteLine("Aluguel #" + (i+1) + ":");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int num = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                vect[num] = new Estudante(nome, email, num);
+            }
+
+            Console.WriteLine("Quartos ocupados: ");
+            for (int i = 0; i < 10; i++) {
+                if (vect[i] != null) {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+        }
+    }
+}
