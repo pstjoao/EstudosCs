e12d3be314fe29a90d87af976281f4d8c8ddf645
João Pedro Turi <jpturi14@gmail.com>
2025-01-09 15:57:34 -0300

    Exercício matrizes

---
+++ Exmatriz2/Exmatriz2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exmatriz2 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Insira um número M de linhas e N de colunas: ");
+            string[] dimension = Console.ReadLine().Split(' ');
+            int m = int.Parse(dimension[0]);
+            int n = int.Parse(dimension[1]);
+
+            int[,] mat = new int[m, n];
+
+            for (int i = 0; i < m; i++) {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++) {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            Console.Write("Insira um número X pertencente a matriz: ");
+            int x = int.Parse(Console.ReadLine());
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (mat[i, j] == x) {
+                        Console.WriteLine("Position " + i + "," + j + ":");
+                        if (j > 0) {
+                            Console.WriteLine("Left: " + mat[i, j - 1]);
+                        }
+                        if (i > 0) {
+                            Console.WriteLine("Up: " + mat[i - 1, j]);
+                        }
+                        if (j < n - 1) {
+                            Console.WriteLine("Right: " + mat[i, j + 1]);
+                        }
+                        if (i < m - 1) {
+                            Console.WriteLine("Down: " + mat[i + 1, j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
