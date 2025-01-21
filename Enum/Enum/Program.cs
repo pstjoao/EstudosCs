717f1ce90189f251458bccd18047188561fc0620
João Pedro Turi <jpturi14@gmail.com>
2025-01-21 11:25:52 -0300

    Estudos sobre enumeração

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Enum.Entities;
+using Enum.Entities.Enums;
+
+namespace Enum {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Order order = new Order() {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString(); // Para converter de enumeração pra string
+
+            Console.WriteLine(txt);
+        }
+    }
+}
