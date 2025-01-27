1c0b7c0de5cc81b54ad73f8e3315f93eb737e00a
João Pedro Turi <jpturi14@gmail.com>
2025-01-26 21:34:29 -0300

    Estudos sobre herança

---
+++ DemoHeranca/DemoHeranca/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DemoHeranca {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
