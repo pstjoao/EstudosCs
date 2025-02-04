684dae101a8238737845862e2983bb3e4324c9b1
João Pedro Turi <jpturi14@gmail.com>
2025-02-04 16:19:54 -0300

    Exercício utilizando exceções personalizadas

---
+++ ExcecoesPersonalizadas/ExcecoesPersonalizadas/Program.cs
@@ -0,0 +1,39 @@
+﻿using ExcecoesPersonalizadas.Entities;
+using ExcecoesPersonalizadas.Entities.Exceptions;
+
+namespace ExcecoesPersonalizadas {
+    internal class Program {
+        static void Main(string[] args) {
+            try {
+                Console.Write("Room number: ");
+                int roomNumber = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation res = new Reservation(roomNumber, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + res);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation:");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                res.UpdateDates(checkIn, checkOut);
+                Console.WriteLine("Reservation: " + res);
+            }
+            catch (DomainException e) {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+            catch (FormatException e) {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch (Exception e) {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+        }
+    }
+}
