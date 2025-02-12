d29ac98768bfc2a7de8d491f45d9b3456efc8df3
João Pedro Turi <jpturi14@gmail.com>
2025-02-12 20:28:29 -0300

    Estudos sobre File e FileInfo

---
+++ FileAndFileInfo/FileAndFileInfo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace FileAndFileInfo {
+    internal class Program {
+        static void Main(string[] args) {
+
+            string sourcePath = @"c:\file1.txt";
+            string targetPath = @"c:\file2.txt";
+
+            try {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines) {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
