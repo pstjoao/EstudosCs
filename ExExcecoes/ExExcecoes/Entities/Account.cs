7f4b66202ee8f0eb9fd8c2b03765fa05363865f9
João Pedro Turi <jpturi14@gmail.com>
2025-02-12 18:55:43 -0300

    Exercício utilizando exceções personalizadas

---
+++ ExExcecoes/ExExcecoes/Entities/Account.cs
@@ -0,0 +1,36 @@
+﻿using ExExcecoes.Entities.Exceptions;
+
+namespace ExExcecoes.Entities {
+    internal class Account {
+
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithDrawLimit { get; set; }
+
+        public Account() { 
+        }
+
+        public Account(int number, string holder, double balance, double withDrawLimit) {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithDrawLimit = withDrawLimit;
+        }
+
+        public void Deposit(double amount) {
+            Balance += amount;
+        }
+
+        public void WithDraw(double amount) {
+            if (Balance < amount) {
+                throw new DomainException("Not enough balance");
+            }
+            if (WithDrawLimit < amount) {
+                throw new DomainException("The amount exceeds withdraw limit");
+            }
+
+            Balance -= amount;
+        }
+    }
+}
---
+++ ExExcecoes/ExExcecoes/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ExExcecoes.Entities.Exceptions {
+    internal class DomainException : ApplicationException {
+
+        public DomainException(string message) : base(message) { // Construtor recebe um string message e repassa essa
+                                                                 // mensagem para o construtor da classe ApplicationExeception
+        }
+    }
+}
