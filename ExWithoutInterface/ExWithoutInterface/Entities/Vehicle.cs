5f638a106d1221e4b4176d2b8d4afce9e322fd9a
João Pedro Turi <jpturi14@gmail.com>
2025-02-20 19:00:27 -0300

    Exercício sem utilizar interface

---
+++ ExWithoutInterface/ExWithoutInterface/Entities/Vehicle.cs
@@ -0,0 +1,10 @@
+﻿namespace ExWithoutInterface.Entities {
+    internal class Vehicle {
+
+        public string Model { get; set; }
+
+        public Vehicle(string model) {
+            Model = model;
+        }
+    }
+}
