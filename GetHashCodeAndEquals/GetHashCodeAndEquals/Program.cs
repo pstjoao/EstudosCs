fec8d05fa5dd77e103a619f05d3cebfdc39b3ac0
João Pedro Turi <jpturi14@gmail.com>
2025-03-24 12:09:17 -0300

    Estudos sobre GetHashCode e Equals

---
+++ GetHashCodeAndEquals/GetHashCodeAndEquals/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using GetHashCodeAndEquals.Entities;
+
+namespace GetHashCodeAndEquals {
+    internal class Program {
+        static void Main(string[] args) {
+
+            string a = "Maria";
+            string b = "Alex";
+
+            Console.WriteLine(a.Equals(b)); // Retorna "False"
+
+            string c = "Joao";
+            string d = "Joao";
+            Console.WriteLine(c.GetHashCode());
+            Console.WriteLine(d.GetHashCode()); // Gera o mesmo código
+            Console.WriteLine("------------------");
+
+            Client x = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client y = new Client { Name = "Alex", Email = "maria@gmail.com" }; // Os emails são iguais, retorna true
+
+            Console.WriteLine(x.Equals(y));
+            Console.WriteLine(x == y);
+            Console.WriteLine(x.GetHashCode());
+            Console.WriteLine(y.GetHashCode());
+        }
+    }
+}
